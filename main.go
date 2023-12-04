e1402c9edcb14781ee71ec700c1ad432f531ebb2
sam <sementinchurin@gmail.com>
2023-12-04 19:48:45 +0300

    third lesson

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 
+// Number of the lesson: 4
+
+// declearing variables
 var (
 	// declare a variable
 	name = "name"
@@ -11,10 +14,15 @@ var (
 	lastName string
 )
 
+// declearing constants, same method for a few constants like in vars
+// always in lowercase
+const someConstant = 150
+
 func main() {
-	version := 1             // infer integer
+	var version int
+	version = 100            // infer integer
 	otherVersion := "second" // infer string
 	anotherVersion := 10.1   // infer float
-
-	fmt.Println(version, otherVersion, anotherVersion)
+	// stands for format print line
+	fmt.Println(name, version, otherVersion, anotherVersion, someConstant)
 }
