0d39d8a06e2169405718d8b6ea08b98435f7dc75
sam <sementinchurin@gmail.com>
2024-01-03 13:41:10 +0300

    creating modules and building a program

---
+++ go.mod
@@ -0,0 +1,3 @@
+module go_intro_app
+
+go 1.21.4
---
+++ lesson_8.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"go_intro_app/types"
+	"go_intro_app/util"
+)
+
+// --program--
+// 1. something that runs / executes code (go run somefile.go)
+// => main() -> entry function of the program
+// => package main
+
+// --package / library / module--
+// 2. something that someone can import into their program or library
+// -> package <whatever>
+
+// To build program
+// 1. go mod init <program_or_module_name>
+// 2. go build -o <binary_file_name>
+// 3. to run binary - ./binary_file_name
+// if something changed - you need to rebuild
+// the binary file with the same command
+
+// User -> public access
+// user -> private access BUT public in its own package
+
+func main() {
+	// number := getNum ber()
+	user := types.User{
+		Username: util.GetUserName(),
+		Age:      util.GetAge(),
+	}
+
+	fmt.Printf("The user is %+v \n", user)
+}
